b1e13e4967bdb6a862d63e22a1fb02331f3eab84
Eben-Success <aebenezer237@gmail.com>
2022-11-24 13:32:37 +0000

    update

---
+++ modern_business_site/src/components/index.js
@@ -0,0 +1,29 @@
+import { Navbar } from "./Navbar";
+import { Business } from "./Business";
+import { CTA } from "./CTA";
+import { Hero } from "./Hero";
+import Stats from "./Stats";
+import { Billing } from "./Billing";
+import { Button } from "./Button";
+import { CardDeal } from "./CardDeal";
+import { Clients } from "./Clients";
+import { FeedbackCard } from "./FeedbackCard";
+import { Testimonials } from "./Testimonials";
+import Footer from "./Footer";
+import { GetStarted } from "./GetStarted";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    GetStarted,
+    FeedbackCard,
+    Button,
+};
